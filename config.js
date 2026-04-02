49b9dd92f806688e9135a1a5e32fdb648b7b905a
thisarasathsara63-sudo <thisarasathsara63@gmail.com>
2026-04-02 22:39:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/thisarasathsara63-sudo/THISARA-MD/blob/main/images/Orange%20Phoenix%20Animal%20Gaming%20Logo.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 THISARA-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
